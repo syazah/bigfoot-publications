70ef14a969cc6cf41f2e0ca1fc33e2bd4458b7cb
Syed Azaan Ahmad <thesyazah@gmail.com>
2025-03-28 08:05:58 +0530

    adminController

--- backend/src/controllers/admin.controller.ts
+++ backend/src/controllers/admin.controller.ts
@@ -14,35 +14,35 @@ import {
 import bcryptjs from "bcryptjs";
 
 //! SIGNUP ADMIN
-export const AdminSignupController = async (c: Context) => {
-  try {
-    // PRISMA CONFIGURATIONS
-    const prisma = new PrismaClient({
-      datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate());
-    // ACCEPTING BODY
-    const SignUpFormData: AdminSignUpType = await c.req.json();
-    const validate = AdminSignUpSchema.safeParse(SignUpFormData);
-    if (!validate.success) {
-      c.status(400);
-      throw new Error(validate.error.errors[0].message);
-    }
-    //CREATE USER
-    const salt = bcryptjs.genSaltSync(10);
-    const hashedPassword = bcryptjs.hashSync(SignUpFormData.password, salt);
-    const user = await prisma.admin.create({
-      data: {
-        name: SignUpFormData.name,
-        email: SignUpFormData.email,
-        password: hashedPassword,
-      },
-    });
+  export const AdminSignupController = async (c: Context) => {
+    try {
+      // PRISMA CONFIGURATIONS
+      const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+      }).$extends(withAccelerate());
+      // ACCEPTING BODY
+      const SignUpFormData: AdminSignUpType = await c.req.json();
+      const validate = AdminSignUpSchema.safeParse(SignUpFormData);
+      if (!validate.success) {
+        c.status(400);
+        throw new Error(validate.error.errors[0].message);
+      }
+      //CREATE USER
+      const salt = bcryptjs.genSaltSync(10);
+      const hashedPassword = bcryptjs.hashSync(SignUpFormData.password, salt);
+      const user = await prisma.admin.create({
+        data: {
+          name: SignUpFormData.name,
+          email: SignUpFormData.email,
+          password: hashedPassword,
+        },
+      });
 
-    return c.json({ success: true, message: "User Was Created" });
-  } catch (error) {
-    throw error;
-  }
-};
+      return c.json({ success: true, message: "User Was Created" });
+    } catch (error) {
+      throw error;
+    }
+  };
 
 //! SIGN IN CONTROLLER
 export const AdminSignInController = async (c: Context) => {
